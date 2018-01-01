4bc2f938a30346883fcbdb424bcd0a0e8d4d221b
Michael Greene <profmikegreene@gmail.com>
2018-01-01 10:43:48 -0500

    fixed bug on composer install for rating

--- install.sh
+++ install.sh
@@ -13,10 +13,9 @@ echo "============Getting IMSGlobal/LTI-Sample-Tool-Provider-PHP:master=========
 git clone https://github.com/IMSGlobal/LTI-Sample-Tool-Provider-PHP.git rating/www
 
 echo "============Configuring rating============"
-docker run --rm -v $(pwd)/rating/www:/app composer:latest install
 mv -v rating/www/src/* rating/www/
 cp rating/config.php rating/www
-
+docker run --rm -v $(pwd)/rating/www:/app composer:latest install
 
 echo "============Cleaning up rating============"
 rm -rf ./rating/www/.git
