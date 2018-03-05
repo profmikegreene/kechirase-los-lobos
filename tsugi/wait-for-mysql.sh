a0c28ebeea7bc57248a0fb04723307f38d018464
Michael Greene <profmikegreene@gmail.com>
2018-03-05 15:20:23 -0500

    trying to keep the server alive

--- tsugi/wait-for-mysql.sh
+++ tsugi/wait-for-mysql.sh
@@ -12,3 +12,11 @@ echo -e "\nmysql ready"
 
 echo "Running tsugi config"
 php config.php
+
+# from https://github.com/docker-library/php/blob/fd8e15250a0c7667b161c34a25f7916b01f72367/7.2/stretch/apache/docker-php-entrypoint
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- apache2-foreground "$@"
+fi
+
+exec "$@"
